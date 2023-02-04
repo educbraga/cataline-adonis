d0438e746f3517fd918000dfb729210b0d74757c
Eduardo Braga <eduardocbraga@hotmail.com>
2023-02-04 13:40:48 -0300

    feat: cria controller de Posts

---
+++ app/Controllers/Http/PostsController.ts
@@ -0,0 +1,7 @@
+// import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class PostsController {
+  public index() {
+    return ['post 1', 'post 2', 'post 3']
+  }
+}
