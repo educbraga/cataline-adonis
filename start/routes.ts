54da186f670d1f4933776a617e50f0c59052fb58
Eduardo Braga <eduardocbraga@hotmail.com>
2023-02-10 21:36:22 -0300

    feat: Controller de Posts

--- start/routes.ts
+++ start/routes.ts
@@ -20,4 +20,8 @@
 
 import Route from '@ioc:Adonis/Core/Route'
 
-Route.get('/', 'PostsController.index')
+Route.get('/', async () => {
+	return { hello: 'world' }
+})
+
+Route.resource('/posts', 'PostsController').apiOnly()
